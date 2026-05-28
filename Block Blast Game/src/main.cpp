4bc03fe5f6ea56e8a585602dd5a3cf737cea8392
Nate Newhart <natenewhart@ucsb.edu>
2026-05-28 10:55:56 -0700

    remove comments

--- Block Blast Game/src/main.cpp
+++ Block Blast Game/src/main.cpp
@@ -25,7 +25,3 @@ int main()
 	return 0;
 }
 
-// BIG TODO:
-/*
-* Implement a way to store game data and settings in a file and load it when the game starts, and save it when the game closes. This will allow for things like saving high scores, player progress, and game settings.
-*/
