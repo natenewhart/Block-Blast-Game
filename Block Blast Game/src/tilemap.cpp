04f177fa3c0fee4be526f326edcbc09db299ec8b
Nate <natenewhart@gmail.com>
2026-07-18 16:41:13 -0700

    Remove all unused code in tilemap

--- Block Blast Game/src/tilemap.cpp
+++ Block Blast Game/src/tilemap.cpp
@@ -13,13 +13,10 @@ TileMap::TileMap(sf::Vector2f position)
 	, mGridVertices{ sf::Vertex(), sf::Vertex(), sf::Vertex(), sf::Vertex() }
 	, mWidth(GameSettings::Get().tileMap.width)
 	, mHeight(GameSettings::Get().tileMap.height)
-	//, mTiles(mHeight * mWidth, Tile{ sf::Color::Transparent, sf::Color::Transparent, false })
 	, mGrid()
 	, mPosition(position)
 	, mcBlockSearchAreaSize(2)
 	, mcSearchAreaWidth(InitSearchAreaWidth(mcBlockSearchAreaSize))
-	//, mFullRows(mHeight, false)
-	//, mFullCols(mWidth, false)
 	, mSubmitBuffer{std::vector<sf::Vector2i>(), std::vector<bool>(mHeight, false), std::vector<bool>(mWidth, false), sf::Color::Transparent}
 	, mTileColors(mHeight * mWidth, sf::Color::Transparent)
 {
@@ -50,14 +47,6 @@ int TileMap::InitSearchAreaWidth(int blockSearchAreaSize) const
 
 void TileMap::Clear()
 {
-	//for (auto& color : mTileColors)
-	//{
-	//	mTileCol
-	//	tile.isOccupied = false;
-	//	tile.color   = sf::Color::Transparent; // Placeholder color for empty tile
-	//	tile.overlayColor = sf::Color::Transparent; // Placeholder color for empty tile overlay
-	//}
-
 	std::fill(mTileColors.begin(), mTileColors.end(), sf::Color::Transparent);
 	mGrid.Clear();
 
@@ -105,13 +94,6 @@ void TileMap::CheckFullLines(Grid& grid)
 	}
 }
 
-
-//void TileMap::PlaceBlockOverlay()
-//{
-//	PlaceBlockOnTileMapOverlay(); // Place block overlay on tilemap for block placement preview, returns true if block is placeable and overlay was placed successfully, false if block is not placeable and overlay was not placed
-//	HighlightFullLines();
-//}
-
 void TileMap::PlaceBlock()
 {
 	PlaceBlockOnGrid(mGrid);
@@ -135,25 +117,6 @@ void TileMap::ClearFullLines(Grid& grid)
 			grid.ClearTile(col, row);
 	}
 }
-
-//void TileMap::HighlightFullLines()
-//{
-//	sf::Color blockColor = mSubmitBuffer.activeBlockColor;
-//	blockColor.a = 128; // Set alpha to 50% for overlay
-//
-//	for (int row = 0; row < mHeight; row++)
-//	{
-//		if (!mSubmitBuffer.rowsToClear[row]) continue;
-//		for (int col = 0; col < mWidth; col++)
-//			mTiles[IndexTiles(row, col)].overlayColor = blockColor;
-//	}
-//	for (int col = 0; col < mWidth; col++)
-//	{
-//		if (!mSubmitBuffer.colsToClear[col]) continue;
-//		for (int row = 0; row < mHeight; row++)
-//			mTiles[IndexTiles(row, col)].overlayColor = blockColor;
-//	}
-//}
 
 // ------------------- Block Placement Functions -------------------
 
@@ -247,21 +210,10 @@ void TileMap::PlaceBlockColors()
 	}
 }
 
-//void TileMap::PlaceBlockOnTileMapOverlay()
-//{
-//	for (sf::Vector2i tilePos : mActiveBlockTilePositions)
-//	{
-//		sf::Color blockColor = mActiveBlockColor;
-//		blockColor.a = 128; // Set alpha to 50% for overlay
-//		mTiles[IndexTiles(tilePos)].overlayColor = blockColor;
-//	}
-//}
-
 // ------------------- Block Inventory Spawning Functions ---------------------
 
 Block::tViewHand TileMap::CreateRandomBlockHand()
 {
-	//std::vector<bool> currHandTileMap = CopyTileMapToBoolean();
 	Grid currGrid = mGrid; // Create copy of current grid to simulate block placements on for hand creation
 	Block::tViewHand blockHand;
 
@@ -270,7 +222,6 @@ Block::tViewHand TileMap::CreateRandomBlockHand()
 	{
 		tries++; assert(tries < 100); // DEBUG
 		Block::View nextBlock = GetRandomBlockView();
-		//std::vector<sf::Vector2i> blockTilePositions;
 		if (!TryPlaceBlockView(currGrid, nextBlock, mSubmitBuffer.tilePositions)) // If block cannot be placed skip this iteration
 		{
 			continue;
@@ -282,9 +233,7 @@ Block::tViewHand TileMap::CreateRandomBlockHand()
 		{
 			PlaceBlockOnGrid(currGrid);
 			CheckFullLines  (currGrid);
-			//ClearFullLines  (currGrid);
 		}
-			//SubmitBlockView(currHandTileMap, blockTilePositions);
 	}
 	ClearSubmittedBlockCache();
 	return blockHand;
@@ -328,7 +277,6 @@ bool TileMap::TryPlaceBlockView(const Grid& grid, Block::View& outBlock, std::ve
 	for (int i = 0; i < tilesIndices.size(); i++) // Iterate over tilemap randomly
 	{
 		sf::Vector2i originTile = grid.ToTilePos(tilesIndices[i]);
-		//if (IsBlockViewPlaceable(tileMap, tilePositions, originTile))
 		if (IsBlockPlaceable(grid, tilePositions, originTile)) // Check if block can be placed at this tile position
 		{
 			outBlock.position = TilePosToPixelPos(originTile);
@@ -350,72 +298,6 @@ std::vector<int> TileMap::GetOpenTileIndices(const Grid& grid) const
 	}
 	return openTiles;
 }
-
-//bool TileMap::IsBlockViewPlaceable(const std::vector<bool>& tileMap, const std::vector<sf::Vector2i>& blockTilePositions, sf::Vector2i tileOrigin) const
-//{
-//	for (sf::Vector2i blockTilePos : blockTilePositions)
-//	{
-//		blockTilePos += tileOrigin;
-//		if (!IsTilePosition(blockTilePos) || tileMap[IndexTiles(blockTilePos)] == true) return false;
-//	}
-//	return true;
-//}
-//
-//void TileMap::PlaceBlockView(std::vector<bool>& tileMap, const std::vector<sf::Vector2i>& blockTilePositions)
-//{
-//	for (sf::Vector2i blockTilePos : blockTilePositions)
-//	{
-//		tileMap[IndexTiles(blockTilePos)] = true;
-//	}
-//}
-
-//std::vector<bool> TileMap::CopyTileMapToBoolean()
-//{
-//	std::vector<bool> tileMap;
-//	tileMap.reserve(mTiles.size());
-//	for (int i = 0; i < mTiles.size(); i++)
-//	{
-//		tileMap.emplace_back(mTiles[i].isOccupied);
-//	}
-//	return tileMap;
-//}
-
-//void TileMap::SubmitBlockView(std::vector<bool>& tileMap, const std::vector<sf::Vector2i>& blockTilePositions)
-//{
-//	PlaceBlockView(tileMap, blockTilePositions);
-//
-//	for (int i = 0; i < tileMap.size(); i++)
-//	{
-//		int row = i / mWidth;
-//		int col = i % mWidth;
-//
-//		// Check row
-//		for (int c = 0; c < mWidth; c++)
-//		{
-//			if (!tileMap[IndexTiles(row, c)]) break;
-//			if (c == mWidth - 1)
-//			{
-//				for (int j = 0; j < mWidth; j++) // Clear row
-//				{
-//					tileMap[IndexTiles(row, j)] = true;
-//				}
-//			}
-//		}
-//
-//		// Check column
-//		for (int r = 0; r < mHeight; r++)
-//		{
-//			if (!tileMap[IndexTiles(r, col)]) break;
-//			if (r == mHeight - 1)
-//			{
-//				for (int j = 0; j < mHeight; j++) // Clear Column
-//				{
-//					tileMap[IndexTiles(j, col)] = true;
-//				}
-//			}
-//		}
-//	}
-//}
 
 float TileMap::WeighBlockViewHand(const Block::tViewHand& blockHand)
 {
@@ -448,15 +330,6 @@ Block::tHand TileMap::ConvertToBlockHand(const Block::tViewHand& other)
 
 // ------------------- Private Helper Functions -------------------
 
-bool TileMap::IsBlockNearPlaceable(sf::Vector2f blockPosition) const
-{
-	int extraTiles = mcBlockSearchAreaSize - 1; // Number of tiles in search area around block position
-	sf::Vector2f newPos = mPosition - (float)extraTiles * GameSettings::Get().tile.size; // Top left corner of search area around block position
-	sf::Vector2f tileMapScale = sf::Vector2f(mWidth * GameSettings::Get().tile.size.x, mHeight * GameSettings::Get().tile.size.y) + (float)extraTiles * 2.f * GameSettings::Get().tile.size; // Size of tilemap plus search area around block position
-
-	return isWithinRect(newPos, tileMapScale, blockPosition);
-}
-
 sf::Vector2f TileMap::SnapToTile(sf::Vector2f position) const
 {
 	sf::Vector2f relativePos = position - mPosition;
@@ -478,11 +351,6 @@ sf::Vector2f TileMap::TilePosToPixelPos(sf::Vector2i tilePos) const
 {
 	return mPosition + sf::Vector2f(tilePos.x * GameSettings::Get().tile.size.x, tilePos.y * GameSettings::Get().tile.size.y);
 }
-
-//int TileMap::IndexTiles(int row, int col) const
-//{
-//	return row * mWidth + col;
-//}
 
 int TileMap::IndexTiles(sf::Vector2i tilePos) const
 {
@@ -545,7 +413,8 @@ void TileMap::DrawTiles(sf::RenderWindow& window)
 			if (isActiveBlockTile || isActiveRowOrCol)
 			{
 				sf::Color overlayColor = mSubmitBuffer.activeBlockColor;
-				overlayColor.a = 128; // Set alpha to 50% for overlay
+				overlayColor.a = 128; 
+
 				mTileRect.setFillColor(overlayColor);
 				window.draw(mTileRect);
 			}
--- Block Blast Game/src/tilemap.h
+++ Block Blast Game/src/tilemap.h
@@ -17,12 +17,6 @@
 class TileMap
 {
 private:
-	//struct Tile
-	//{
-	//	sf::Color color;
-	//	sf::Color overlayColor;
-	//	bool isOccupied;
-	//};
 	struct SubmissionBuffer
 	{
 		std::vector<sf::Vector2i> tilePositions; // Active block tile positions in tile coordinates relative to tilemap grid
@@ -38,16 +32,11 @@ public:
 
 	bool SubmitBlock(const Block& block); // Cache block placement position, rows to be deleted, and return boolean value if block can be placed
 	
-	void PlaceBlockOverlay(); // Places cached block as highlight to screen and highlights rows and cols that would be deleted
 	void PlaceBlock();        // Places cached block to screen and handles deletion updates
-
 	void Draw(sf::RenderWindow& window); // Draws grid lines and tiles
 
 	Block::tHand CreateBlockHand(); // Create block hand of three blocks based on current tilemap state using block spawning algorithm
 
-	// UNUSED FUNCTION
-	bool IsBlockNearPlaceable(sf::Vector2f blockPosition) const; // Is block close enough to tilemap to warrant a placeability check
-	
 private:
 	void Init(); // Initializes tilemap data and grid vertices
 	int  InitSearchAreaWidth(int blockSearchAreaSize) const; // Initialize search area width constant variable
@@ -56,7 +45,6 @@ private:
 	void DrawGridLines(sf::RenderWindow& window); // Draws grid lines with top left corner at mPosition
 	void DrawTiles    (sf::RenderWindow& window); // Draws tiles
 	
-	//void DeleteTile(int index);          // Deletes a tile at the specified row and column
 	sf::Vector2f SnapToTile(sf::Vector2f position) const;  // Take pixel pos and return position of current tile (top left)
 	sf::Vector2i GetTilePosition(sf::Vector2f screenPosition) const; // Converts position in screen space to the col, row of tilemap which is the tile that position is inside of, returns (-1, -1) if position is outside of tilemap bounds
 	sf::Vector2f TilePosToPixelPos(sf::Vector2i tilePos) const;
@@ -67,11 +55,9 @@ private:
 	bool SetClosestOpenBlockPositions(const Block& block); // Find closest tileMap grid positions for block on grid. Store indices in mBlockPlacementBuffer and return true if block is placeable, and false otherwise
 	void PlaceBlockOnGrid(Grid& grid);        // Places block on tilemap at given grid position by setting tiles at block tile positions to occupied and block color. Returns true if block was placed successfully, false if any tile positions of block were occupied on tilemap
 	void PlaceBlockColors(); // Set tile colors based on tile occupancy, used for drawing tiles
-	//void PlaceBlockOnTileMapOverlay(); // Places block overlay on tilemap at given grid position by setting tiles at block tile positions to occupied and block color with transparency. Used for block placement preview when player is moving block around tilemap. Returns true if block overlay was placed successfully, false if any tile positions of block were occupied on tilemap
 
 	void CheckFullLines(Grid& grid);
 	void ClearFullLines(Grid& grid);
-	//void HighlightFullLines(); // Check for full rows and columns created by highlighted overlay and highlight them the color of active block, used for block placement preview when player is moving block around tilemap.
 
 	// Block Inventory Spawner Private Members, three step process each function having their own helpers
 	Block::tViewHand CreateRandomBlockHand();
@@ -79,8 +65,6 @@ private:
 	Block::View GetRandomBlockView(); // Lightweight function that just does the random finding of block
 	bool TryPlaceBlockView   (const Grid& grid, Block::View& outBlock, std::vector<sf::Vector2i>& tilePositions); // Find position for current block view and set block position. If not placeable return false
 	std::vector<int> GetOpenTileIndices(const Grid& grid) const; // Get indices of open tiles in tilemap
-	bool IsBlockViewPlaceable(const Grid& grid, const std::vector<sf::Vector2i>& blockTilePositions, sf::Vector2i tileOrigin) const; // Check if block view is placeable on custom tileMap parameter
-	void PlaceBlockView      (const Grid& grid, const std::vector<sf::Vector2i>& blockTilePositions); // Place block view on tilemap by setting tiles at block tile positions to occupied and block color. Returns true if block was placed successfully, false if any tile positions of block were occupied on tilemap
 
 	// CreateRandomBlockHand helpers
 	std::vector<bool> CopyTileMapToBoolean();
@@ -91,14 +75,11 @@ private:
 	Block::tHand ConvertToBlockHand(const Block::tViewHand& other);
 
 	// Boolean Helpers 
-	//bool IsTilePosition(sf::Vector2i tilePosition) const;
 	bool IsBlockPlaceable(const Grid& grid, const std::vector<sf::Vector2i>& blockTilePositions, sf::Vector2i tileOrigin) const;
 	bool IsInActiveBlockTilePositions(int col, int row) const;
 
 	// Index conversion helpers
-	//int IndexTiles(int row, int col) const; // Converts 2D grid position to 1D index in mTiles vector
 	int IndexTiles(sf::Vector2i tilePos) const;
-	//sf::Vector2i IndexToTilePos(int index); // Convert index for tilemap to tile position vector
 
 	void ClearSubmittedBlockCache();
 
@@ -111,16 +92,8 @@ private:
 	int mWidth;  // Number of tiles in horizontal direction
 	int mHeight; // Number of tiles in vertical direction
 
-	//std::vector<Tile> mTiles; // Stores tile data in a 2D vector (rows of columns). Important: declared
-	
 	Grid mGrid;
 	std::vector<sf::Color> mTileColors;
-
-	// Submitted block variables
-	//std::vector<sf::Vector2i> mActiveBlockTilePositions; // Stores grid positions of tiles that a block would occupy if placed at a given position, used for checking if block placement is valid and for highlighting tiles when player is moving block around tilemap
-	//std::vector<bool> mFullRows;
-	//std::vector<bool> mFullCols;
-	//sf::Color mActiveBlockColor;
 
 	SubmissionBuffer mSubmitBuffer; // Cache for block placement data to be used when player places block on tilemap after moving it around for preview
 
